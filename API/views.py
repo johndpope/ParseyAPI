5ff3f2ffdae070fa76f97c4f8fa6b26bbb9464d0
dnadler <daniel_nadler@putnam.com>
2016-09-21 13:06:58 -0400

    fixed errors in api views parse() function

--- API/views.py
+++ API/views.py
@@ -1,24 +1,26 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from subprocess import Popen, PIPE
+
 
 # Create your views here.
 def parse(request):
-	input_text = request.GET['text']
-	if 'output_type' in request.GET:
-		ouptut_type = request.GET['output_type']
-	else:
-		output_type = 'tree'
+    input_text = request.GET['text']
+    if 'output_type' in request.GET:
+        output_type = request.GET['output_type']
+    else:
+        output_type = 'tree'
 
-	if output_type == 'tree':
-		output = Popen(["cd ~/models/syntaxnet && echo '{0}' | syntaxnet/demo.sh".format(input_text)], shell=True, stdout=PIPE).communicate()[0]
-	
-	d = {
+    if output_type == 'tree':
+        output = Popen(["cd ~/models/syntaxnet && echo '{0}' | syntaxnet/demo.sh".format(input_text)], shell=True,
+                       stdout=PIPE).communicate()[0]
+        return HttpResponse(output)
 
-		'input_text': input_text,
-		'output': output
-	}
-	
-	if output_type == 'csv':
-		output = Popen(["cd ~/models/syntaxnet && echo '{0}' | syntaxnet/my_demo.sh".format(input_text)], shell=True, stdout=PIPE).communicate()[0]	
-		
-		return JsonResponse(d)
+    if output_type == 'csv':
+        output = Popen(["cd ~/models/syntaxnet && echo '{0}' | syntaxnet/my_demo.sh".format(input_text)], shell=True,
+                       stdout=PIPE).communicate()[0]
+        d = {
+
+            'input_text': input_text,
+            'output': output
+        }
+        return JsonResponse(d)
