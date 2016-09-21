7753acbdc909f6b85e23e75602bbfaaa3c767c80
dnadler <daniel_nadler@putnam.com>
2016-09-21 12:53:34 -0400

    fix string format error

--- API/views.py
+++ API/views.py
@@ -5,7 +5,7 @@ from subprocess import Popen, PIPE
 def parse(request):
     input_text = request.GET['text']
 
-    output = Popen(["cd ~models/syntaxnet && echo '{0}' | syntaxnet/my_demo.sh" % input_text], shell=True, stdout=PIPE).communicate()[0]
+    output = Popen(["cd ~models/syntaxnet && echo '{0}' | syntaxnet/my_demo.sh".format(input_text)], shell=True, stdout=PIPE).communicate()[0]
 
     d = {
         'input_text': input_text,
