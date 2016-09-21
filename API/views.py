222fa55daccc6cf1308bb0842c15a9646ff5db5d
Ubuntu <ubuntu@ip-172-30-1-114.ec2.internal>
2016-09-21 17:03:29 +0000

    server-side edits

--- API/views.py
+++ API/views.py
@@ -3,12 +3,22 @@ from subprocess import Popen, PIPE
 
 # Create your views here.
 def parse(request):
-    input_text = request.GET['text']
+	input_text = request.GET['text']
+	if 'output_type' in request.GET:
+		ouptut_type = request.GET['output_type']
+	else:
+		output_type = 'tree'
 
-    output = Popen(["cd ~models/syntaxnet && echo '{0}' | syntaxnet/my_demo.sh".format(input_text)], shell=True, stdout=PIPE).communicate()[0]
+	if output_type == 'tree':
+		output = Popen(["cd ~/models/syntaxnet && echo '{0}' | syntaxnet/demo.sh".format(input_text)], shell=True, stdout=PIPE).communicate()[0]
+	
+	d = {
 
-    d = {
-        'input_text': input_text,
-        'output': output
-    }
-    return JsonResponse(d)
+		'input_text': input_text,
+		'output': output
+	}
+	
+	if output_type == 'csv':
+		output = Popen(["cd ~/models/syntaxnet && echo '{0}' | syntaxnet/my_demo.sh".format(input_text)], shell=True, stdout=PIPE).communicate()[0]	
+		
+		return JsonResponse(d)
